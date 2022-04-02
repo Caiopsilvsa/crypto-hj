19e3fd262f89148973d1d076db5006a9d75e7309
Caiopsilvsa <caio.psilva@outlook.com>
2022-04-01 21:52:46 -0300

    ghpages config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -10,13 +10,13 @@ import {Page} from './Page'
 function App() {
   return (
   <>
-  <BrowserRouter> 
+  <HashRouter> 
     <GlobalStyle/> 
      <Routes>
-       <Route path="/" element={<Page/>}></Route>
+       <Route path="https://Caiopsilvsa.github.io/crypto-hj/" element={<Page/>}></Route>
      </Routes>
 
-  </BrowserRouter>
+  </HashRouter>
    
   </> 
   );
